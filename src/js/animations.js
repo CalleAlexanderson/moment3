c06073b582f16bdd53c816656cd378112a5a610d
CalleAlexanderson <calle.alexanderson@telia.com>
2024-02-15 21:33:26 +0100

    fixade problem med frågor

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,16 +1,14 @@
 // Globala konstanter och variabler
+let question = 1;
 
 // --------------------------------------------------
 // Initiera globala variabler och händelsehanterare
 function init() {
 
-    let question = 1;
 
     document.getElementById('div1').addEventListener('click', getAnswer);
     document.getElementById('div2').addEventListener('click', resetCards);
-    document.getElementById('question_btn').addEventListener('click', ()=>{
-        changeQuestion(question);
-    });
+    document.getElementById('question_btn').addEventListener('click', changeQuestion);
 
 } // Slut init
 window.addEventListener('load', init);
@@ -33,11 +31,11 @@ function resetCards() {
 }
 
 // satte funktionen till async så jag kan använda timeout
-async function changeQuestion(q) {
-    q = q + 1;
+async function changeQuestion() {
+    question = question + 1;
 
-    if (q == 6) {
-        q = 1;
+    if (question == 6) {
+        question = 1;
     }
 
     if (document.getElementById('div1').classList.contains("animationShowACardQ")) {
@@ -45,33 +43,26 @@ async function changeQuestion(q) {
         await new Promise(resolve => setTimeout(resolve, 3000)); //sätter en timeout på 3 sekunder så korten hinner gå igenom sin animation
     }
 
-    switch (q) {
+    switch (question) {
         case 2:
-            console.log("Fråga 2");
             document.getElementById('div1_p').innerText = 'På vilket hip hop album kan låten "Many men" hittas?';
             document.getElementById('div2_p').innerText = "Get rich or die tryin'";
             break;
         case 3:
-            console.log("Fråga 3");
             document.getElementById('div1_p').innerText = "Disstracket No vaseline är riktad mot vilken grupp?";
             document.getElementById('div2_p').innerText = "N.W.A";
 
             break;
         case 4:
-            console.log("Fråga 4");
             document.getElementById('div1_p').innerText = "Vilka artister förutom Eminem samarbetade på låten bump heads?";
             document.getElementById('div2_p').innerText = "50 Cent, Tony Yayo, Lloyd Banks";
             break;
         case 5:
-            console.log("Fråga 5");
             document.getElementById('div1_p').innerText = "Disstracket killshot var riktad mot vilken rappare?";
             document.getElementById('div2_p').innerText = "Machine Gun Kelly";
 
             break;
-
-
         default:
-            console.log("Fråga 1");
             document.getElementById('div1_p').innerText = "Vad hette debut albumet från hip hop gruppen N.W.A?";
             document.getElementById('div2_p').innerText = "Straight Outta Compton";
             break;
