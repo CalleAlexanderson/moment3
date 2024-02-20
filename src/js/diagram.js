69e9779ad0f0a2095f47d4c94604ead2d5923ada
CalleAlexanderson <calle.alexanderson@telia.com>
2024-02-20 20:44:22 +0100

    stapeldiagram klar, cirkel diagram påbörjad

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -3,15 +3,6 @@
 // --------------------------------------------------
 // Initiera globala variabler och händelsehanterare
 function init() {
-    let data = {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-        series: [
-            [5, 2, 4, 2, 10]
-        ]
-    };
-
-    new Chartist.Line('#chart2', data);
-
 
     fetchData();
 
@@ -41,7 +32,7 @@ async function makeGraphs(gData) {
         if (gData[index].admissionRound == "HT2023") {
             if (gData[index].type == "Kurs") {
                 courses.push(gData[index])
-            } else{
+            } else {
                 programs.push(gData[index])
             }
         }
@@ -51,13 +42,80 @@ async function makeGraphs(gData) {
     console.log(courses);
     courses = courses.sort((b, a) => a.applicantsTotal - b.applicantsTotal);
 
+    programs = programs.sort((b, a) => a.applicantsTotal - b.applicantsTotal);
+
+    let barOptions = {
+        labelInterpolationFnc: function (value) {
+            return value[0]
+        },
+        reverseData: true,
+        horizontalBars: true,
+        fullWidth: false,
+    };
+
+    let responsiveOptionsBar = [
+        ['screen and (min-width: 0)', {
+            axisY: {
+                offset: 75,
+                showGrid: false,
+            },
+            axisX: {
+                // showGrid: false,
+            }
+        }],
+        ['screen and (min-width: 1024px)', {
+            axisY: {
+                offset: 200,
+            }
+        }]
+    ];
+
     let barChartData = {
+        label: 'sökande',
         labels: [courses[0].name, courses[1].name, courses[2].name, courses[3].name, courses[4].name, courses[5].name],
         series: [
             [courses[0].applicantsTotal, courses[1].applicantsTotal, courses[2].applicantsTotal, courses[3].applicantsTotal, courses[4].applicantsTotal, courses[5].applicantsTotal]
         ]
     };
-    
-    new Chartist.Bar('#chart1', barChartData);
-    // console.log(gData);
+
+    new Chartist.Bar('#chart1', barChartData, barOptions, responsiveOptionsBar);
+
+    let data = {
+        labels: [
+            `${programs[0].name}, ${programs[1].applicantsTotal} personer sökte programmet`,
+            `${programs[1].name}, ${programs[2].applicantsTotal} personer sökte programmet`,
+            `${programs[2].name}, ${programs[3].applicantsTotal} personer sökte programmet`,
+            `${programs[3].name}, ${programs[4].applicantsTotal} personer sökte programmet`,
+            `${programs[4].name}, ${programs[5].applicantsTotal} personer sökte programmet`],
+        series: [programs[0].applicantsTotal, programs[1].applicantsTotal, programs[2].applicantsTotal, programs[3].applicantsTotal, programs[4].applicantsTotal]
+    };
+
+    let pieOptions = {
+        labelInterpolationFnc: function (value) {
+            return value[0]
+        }
+    };
+
+    let responsiveOptionsPie = [
+        ['screen and (min-width: 0)', {
+            chartPadding: 10,
+            labelOffset: 100,
+            labelDirection: 'explode',
+            labelInterpolationFnc: function (value) {
+                return value;
+            }
+        }],
+        ['screen and (min-width: 640px)', {
+            chartPadding: 20,
+            labelOffset: 100,
+            labelDirection: 'explode'
+        }],
+        ['screen and (min-width: 1024px)', {
+            chartPadding: 30,
+            labelOffset: 0,
+            labelDirection: 'explode'
+        }]
+    ];
+
+    new Chartist.Pie('#chart2', data, pieOptions, responsiveOptionsPie);
 }
